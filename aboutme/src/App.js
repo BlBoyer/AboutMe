7d50b32ae62ad2f5b3c8b6cd797dc1b910f9db43
BLBoyer <boyer.benlee@gmail.com>
2022-02-20 18:14:40 -0800

    changed styling for buttons and text rendering

--- aboutme/src/App.js
+++ aboutme/src/App.js
@@ -2,43 +2,60 @@ import { useState, useEffect } from 'react';
 //make object.keys the first variable used to populate the list, then change the array to object.values much easier
 function App() {
   const [data, setData]=useState({id:0, name:'', skills:[], likes:[], contact:''});
-  const [meDisp, setMeDisp]=useState([...Object.keys(data)]);
-  const changeProp = (_index, _item) => {
-    var mutated = meDisp;
-    //if the display item is a key of the object, set it to the value
-    if (Object.keys(data).includes(_item)) {
-      //if the value is an array format it for reading
-      (typeof(data[_item])==='object') ? mutated.splice(_index, 1, data[_item].join(', ')) : mutated.splice(_index, 1, data[_item]);
-      setMeDisp([...mutated]); 
-    } else {
-      mutated.splice(_index,1,Object.keys(data)[_index]);
-      setMeDisp([...mutated]);
-    }
-  }
-  useEffect(()=>{
- //api fetch goes here
- //Note: template for actions is 'controller/action/id?'
- const fetchMe = async () => {
-  const response = await fetch("https://localhost:7010/Owner/aboutme");
-  let myData;
-  try {
-    myData = await response.json();
-    setData(myData);
-  } catch(err) {
-    alert(err);
-  }
-  console.log(myData);
-  }
-  fetchMe();
-  setMeDisp(Object.keys(data));
-  },[]);
-  return (
+	const [meDisp, setMeDisp] = useState([...Object.keys(data)]);
+	const changeProp = (_index, cb) => {
+		//if the display item is a key of the object, set it to the value
+		var el = document.getElementById(`cues${_index}`);
+		var detailRow = document.getElementById('content');
+		if (!cb) {
+			var keys = Object.keys(data);
+			var childP = document.createElement('p');
+			el.classList.add('transitive');
+			var txt = '';
+			//if the value is an array format it for reading
+			typeof data[keys[_index + 1]] === 'object'
+				? (txt = data[keys[_index + 1]].join(', '))
+				: (txt = data[keys[_index + 1]]);
+			el.style.opacity = 0.4;
+			el.style.paddingRight = '15px';
+			el.textContent += '  ->';
+			detailRow.appendChild(childP).textContent = txt;
+		} else {
+			el.style.opacity = 1;
+			el.style.paddingRight = '.5em';
+			el.textContent = el.textContent.slice(0, -4);
+			detailRow.removeChild(detailRow.lastChild);
+		}
+	};
+	useEffect(() => {
+		//api fetch goes here
+		//Note: template for actions is 'controller/action/id?'
+		const fetchMe = async () => {
+			const response = await fetch('https://localhost:7010/Owner/aboutme');
+			let myData;
+			try {
+				myData = await response.json();
+				setData(myData);
+			} catch (err) {
+				alert(err);
+			}
+			console.log(myData);
+		};
+		fetchMe();
+		setMeDisp(Object.keys(data));
+	}, []);
+	return (
 		<div id="content">
-			<h1>About Me</h1>
-			<div>
-				<ul>
+			<h1 id="header">About Me</h1>
+			<div id="traitList">
+				<ul id="prop-list">
 					{meDisp.slice(1).map((item, index) => (
-						<li id={'cues' + index} key={index} className="tag" onClick={() => changeProp(index + 1, item)}>
+						<li
+							id={'cues' + index}
+							key={index}
+							className="tag"
+							onMouseEnter={() => changeProp(index, false)}
+							onMouseLeave={() => changeProp(index, true)}>
 							{item.toString()}
 						</li>
 					))}
